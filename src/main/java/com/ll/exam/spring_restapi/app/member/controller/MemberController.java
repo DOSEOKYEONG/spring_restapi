72ce85ad2c8ef31859a896b4fd486eabd0efff4a
DOSEOKYEONG <dho88dho@naver.com>
2022-10-15 19:29:39 +0900

    10 end

---
+++ src/main/java/com/ll/exam/spring_restapi/app/member/controller/MemberController.java
@@ -0,0 +1,44 @@
+package com.ll.exam.spring_restapi.app.member.controller;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    @Data
+    public static class LoginDto {
+        private String username;
+        private String password;
+
+        public boolean isNotValid() {
+            return username == null || password == null || username.trim().length() == 0 | username.trim().length() == 0;
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
+        if (loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authentication", "qwe");
+
+
+        String body = "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+
+        return new ResponseEntity<>(body, headers, HttpStatus.OK);
+    }
+}
